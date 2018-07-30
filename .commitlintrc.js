472cfbd7aad4030269a370012bd6e6ec670db5c8
Masafumi Koba <ybiquitous@gmail.com>
2018-07-30 09:43:42 +0900

    chore: change commitlintrc format (js -> yaml) (#123)

--- .commitlintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-
-  rules: {
-    "scope-enum": [2, "always", ["deps", "readme", "demo", "release", "travis", "sideci"]],
-  },
-};
