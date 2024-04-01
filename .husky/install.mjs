56051beb141339c52b41497dd689a4275c678b51
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-04-01 14:48:03 +0900

    ci: skip husky install on CI
    See https://typicode.github.io/husky/how-to.html#ci-server-and-docker

---
+++ .husky/install.mjs
@@ -0,0 +1,8 @@
+// See https://typicode.github.io/husky/how-to.html#ci-server-and-docker
+
+// Skip Husky install in production and CI
+if (process.env.NODE_ENV === "production" || process.env.CI === "true") {
+  process.exit(0);
+}
+const husky = (await import("husky")).default;
+console.log(husky());
