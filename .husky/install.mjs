5aa63a73aac4b3a1a8ccc07001a6336c682afe3f
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2024-04-01 15:09:03 +0900

    Revert "ci: skip husky install on CI"
    This reverts commit 56051beb141339c52b41497dd689a4275c678b51.

--- .husky/install.mjs
+++
@@ -1,8 +0,0 @@
-// See https://typicode.github.io/husky/how-to.html#ci-server-and-docker
-
-// Skip Husky install in production and CI
-if (process.env.NODE_ENV === "production" || process.env.CI === "true") {
-  process.exit(0);
-}
-const husky = (await import("husky")).default;
-console.log(husky());
